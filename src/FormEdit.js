87d52d0a52fd3c5899a29c8fb96d88a6a71b1cdf
John Stendardo <johnakasquib96@gmail.com>
2020-06-03 16:12:30 -0400

    added constructor, edit, and save

--- src/FormEdit.js
+++ src/FormEdit.js
@@ -18,51 +18,9 @@ class FormEdit extends Component{
             this.baseState=this.state;
             this.setState({edit: false});
         }
-    cancel=()=>{
-        this.setState({...this.baseState, edit:false});
-    }
-    changeName=(event)=>{
-        //console.log("base", this.baseState);
-        //console.log("state",this.state);
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    }
 
     render(){
-            let showEdit=this.state.edit===false ? (
-                <div>
-                    <div className="firstname">First Name:{this.state.firstName}
-                    </div>
-                    <div className="lastname">Last Name: {this.state.lastName}
-                    </div>
-                    <button onClick={this.edit}>EDIT</button>
-                </div>
-                ) : (
-                <div>
-                    <input
-                    className="FNChange"
-                    type="text"
-                    name="firstName"
-                    onChange={this.changeName}
-                    placeholder={this.state.firstName}
-                    />
-                    <input
-                    className="LNChange"
-                    type="text"
-                    name="lastName"
-                    onChange={this.changeName}
-                    placeholder={this.state.lastName}
-                    />
-                    <button className="saveButton" onClick={this.save}>
-                        SAVE
-                    </button>
-                    <button className="cancelButton" onClick={this.cancel}>
-                        CANCEL    
-                    </button>
-                </div>
-            );
-            return <div className="display container">{showEdit}</div>
+            
         }
 }
 export default FormEdit;
